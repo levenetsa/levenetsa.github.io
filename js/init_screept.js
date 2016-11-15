5bfc6c1fb7b4d57dd4e5162061998449f1bdf18f
levenetsa <nike240595@gmail.com>
2016-11-15 19:07:19 +0300

    -Added lines between paragfaphs -Added navigation

---
+++ js/init_screept.js
@@ -0,0 +1,18 @@
+/**
+ * Created by alevenet on 15.11.2016.
+ */
+//nice section initiation
+
+
+(function($){
+    $(function(){
+        $('.parallax').parallax();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+//set size of section 2
+//var actualWimdowHeight = document.getElementById("video").clientHeight - document.getElementsByClassName("footer")[0].clientHeight
+//document.getElementsByClassName("section2")[0].style.minHeight = "" + actualWimdowHeight + "px";
+
