8c024b854514e897214c9389a78c41e63751715b
levenetsa <nike240595@gmail.com>
2016-11-15 22:30:44 +0300

    Simplyfing parallax

--- js/init_screept.js
+++ js/init_screept.js
@@ -11,8 +11,3 @@
 })(jQuery); // end of jQuery name space
 
 
-
-//set size of section 2
-//var actualWimdowHeight = document.getElementById("video").clientHeight - document.getElementsByClassName("footer")[0].clientHeight
-//document.getElementsByClassName("section2")[0].style.minHeight = "" + actualWimdowHeight + "px";
-
